1774184334e40ed5198fe96bd471e29385839df8
maria eduarda <mariaeduardamartinssoares8@gmail.com>
2024-10-01 10:54:23 -0300

    modificado app

--- app/App.tsx
+++ app/App.tsx
@@ -3,10 +3,6 @@ import {GluestackUIProvider} from '@gluestack-ui/themed';
 import {config} from '@gluestack-ui/config';
 import StartNavigator from './res/screen/home-page/navigator';
 
-
-
-
-
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
