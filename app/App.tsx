22fd72f7f6638fc74dad07398ca933bfc4fae4ef
anaa <anakwzy@gmail.com>
2024-10-01 11:19:16 -0300

    acabei a tela database

--- app/App.tsx
+++ app/App.tsx
@@ -4,10 +4,6 @@ import {GluestackUIProvider} from '@gluestack-ui/themed';
 import {config} from '@gluestack-ui/config';
 import StartNavigator from './res/screen/home-page/navigator';
 
-
-
-
-
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
