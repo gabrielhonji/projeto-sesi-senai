3dd435e1b93e1085e5f11f200e3a1462e03dbe5e
ana <carolina.decol2017@gmail.com>
2024-10-01 15:25:31 -0300

    alterações

--- app/App.tsx
+++ app/App.tsx
@@ -3,11 +3,12 @@ import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import React from 'react';
 import StartNavigator from './res/screen/home-page/navigator';
+import PedreiroScreen from './res/screen/pedreiro-page/pedreiro-page';
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      <StartNavigator />
+      <PedreiroScreen/>
     </GluestackUIProvider>
   );
 }
