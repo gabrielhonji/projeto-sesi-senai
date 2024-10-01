8d9798a1eaaaa66386b14c7db380c814d18273b4
Vitor Sossolote <vitorchg11@gmail.com>
2024-10-01 14:38:45 -0300

    Atleta 1st page

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {GluestackUIProvider} from '@gluestack-ui/themed';
 import {config} from '@gluestack-ui/config';
 import StartNavigator from './res/screen/home-page/navigator';
+import AtletaScreen from './res/screen/atleta-page/atleta-page';
 
 
 
