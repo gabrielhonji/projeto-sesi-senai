a33fddb262ad2eb2503074e087f5b41f6ec1d89e
Matheuuuuuuuus <126706433+Matheuuuuuuuus@users.noreply.github.com>
2024-10-25 09:02:19 -0300

    Fix stuff

--- app/res/screen/home-page/navigator.jsx
+++ app/res/screen/home-page/navigator.jsx
@@ -17,7 +17,6 @@ import DireitoScreen from '../grupo-7/direito/direito-page';
 import Home from './home';
 import NutricaoScreen from '../grupo-2/nutricao/nutricao-page';
 import PedreiroScreen from '../grupo-8/pedreiro/pedreiro-page';
-import ArquiteturaScreen from '../arquitetura-page/arquitetura-page';
 import PublicidadeScreen from '../grupo-3/publicidade/publicidade-page';
 // import CabeleireiroScreen from "../cabeleireiro-page/Principal"; PROBLEMA NA BIBLIOTECA RNEUI
 import OnBoarding from './onboarding';
@@ -25,7 +24,7 @@ import GeologoScreen from '../grupo-7/geologo-page/geologo-page';
 import DatabaseScreen from '../grupo-3/database/database-page';
 import JornalistaPage from '../jornalista-page/jornalista-page';
 import EngenhariaScreen from '../grupo-2/nutricao/engenharia-page';
-
+import ArquiteturaScreen from '../grupo-8/arquitetura-page/arquitetura-page';
 
 export default function StartNavigator() {
     return (
