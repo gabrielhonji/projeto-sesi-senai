083ac3e68b8f04e8f3401b795cf2cfdbd2b883b6
Vitor Sossolote <vitorchg11@gmail.com>
2024-10-29 07:31:14 -0300

    :sparkles:feat(front-end): remodelação Home

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,13 @@ import React from 'react';
 import Home from './res/screen/home-page/home';
 
 import StartNavigator from './res/screen/home-page/navigator';
+import { LogBox } from 'react-native';
 
+// Ignore log notification by message
+LogBox.ignoreLogs(['Warning: ...']);
+
+//Ignore all log notifications
+LogBox.ignoreAllLogs();
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
